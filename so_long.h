99d8e3deb3d390fa3da7bdb17a8eff5bb4b0337d
Jeremie Bidaux <jbidaux@c1r11s7.42lausanne.ch>
2023-11-14 17:40:44 +0100

    added sprite

---
+++ so_long.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbidaux <jeremie.bidaux@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 16:17:31 by jbidaux           #+#    #+#             */
+/*   Updated: 2023/11/14 16:19:56 by jbidaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <fcntl.h>
+# include <string.h>
+# include <limits.h>
+# include "mlx/mlx.h"
+# include "libft/libft.h"
+
+#endif // SO_LONG_H
