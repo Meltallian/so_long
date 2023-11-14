1decf4ae7c3c0a730433d5ef86157e4ab5638590
Jeremie Bidaux <jbidaux@c1r11s7.42lausanne.ch>
2023-11-14 18:45:11 +0100

    updated sprite

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jbidaux <jeremie.bidaux@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 10:55:38 by jbidaux           #+#    #+#             */
-/*   Updated: 2023/11/14 17:39:13 by jbidaux          ###   ########.fr       */
+/*   Updated: 2023/11/14 17:43:21 by jbidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	main(void)
 	t_data	img;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 50, 50, "Hello world!");
+	mlx_win = mlx_new_window(mlx, 50, 50, "Pog");
 	img.img = mlx_new_image(mlx, 50, 50);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
