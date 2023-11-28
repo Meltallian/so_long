4ba345abd80d27512ae87541e586d1522679df73
Jeremie Bidaux <jbidaux@c1r11s5.42lausanne.ch>
2023-11-28 18:13:57 +0100

    added .ber check

--- main.c
+++ main.c
@@ -6,11 +6,26 @@
 /*   By: jbidaux <jeremie.bidaux@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 14:23:45 by jbidaux           #+#    #+#             */
-/*   Updated: 2023/11/28 17:10:07 by jbidaux          ###   ########.fr       */
+/*   Updated: 2023/11/28 18:13:45 by jbidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int	check_ber(t_map *game)
+{
+	const char	*ber_ext = ".ber";
+	size_t ber_len;
+	size_t filename_len;
+
+	filename_len = ft_strlen(game->ber);
+	ber_len = ft_strlen(ber_ext);
+	if (filename_len >= ber_len
+		&& ft_strncmp(game->ber + filename_len - ber_len, ber_ext, ber_len) == 0)
+			return (1);
+	else
+		return (0);
+}
 /*
 int	main(int ac, char **av)
 {
@@ -32,6 +47,11 @@ int	main(void)
 	t_map	game;
 
 	game.ber = "map.ber";
+	if(check_ber(&game) == 0)
+	{
+		ft_printf("it ain't a .ber file mon reuf");
+		return (0);
+	}
 	init(&game);
 	destroy(&game);
 	return (0);
